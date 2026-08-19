ba4be7f08546966a778043ce60dec0e829a8d1fe
Mulla Md Kamran <143728592+MullaKamran@users.noreply.github.com>
2026-08-19 16:14:53 +0530

    Day 4: add_and_remove and get_value functions

---
+++ day4_lists_dicts.py
@@ -0,0 +1,16 @@
+# def add_and_remove(items, to_add, to_remove):
+#     items.append(to_add)
+#     items.remove(to_remove)
+#     return items
+
+# print(add_and_remove([1, 2, 3], 4, 2))
+
+def get_value(data, key):
+    if key in data:
+        return data[key]
+    else:
+        return "Not found"
+
+person = {"name": "Kamran", "age": 22}
+print(get_value(person, "name"))
+print(get_value(person, "email"))
